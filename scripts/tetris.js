b1c66b9347200a8d42fb0a02805496f9dcf0a4e4
Dan Barbour <robotd4n@gmail.com>
2017-08-17 16:12:13 -0400

    Updates to components and game logic

--- scripts/tetris.js
+++ scripts/tetris.js
@@ -7,11 +7,11 @@ var Game = {
   processInput() {
 
   },
-  update(delta) {
-    Gameboard.tick(delta);
+  update(timeStep) {
+    Gameboard.tick(timeStep);
   },
   render(interp) {
-    console.log(interp);
+    //console.log(interp);
   },
   main: function(timestamp) {
     var elapsed = timestamp - this.lastFrameMs;
@@ -20,7 +20,7 @@ var Game = {
 
     this.processInput();
     while (this.delta >= this.timeStep) {
-      this.update(this.delta);
+      this.update(this.timeStep);
       this.delta -= this.timeStep;
     }
     this.render(this.delta / this.timeStep);
@@ -31,41 +31,74 @@ var Game = {
 
 // The game board state
 var Gameboard = {
+  properties: {
+    speed: 1
+  },
   board: [
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]]
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0]
   ],
   tetromino: null,
-  tick() {
-    setTetromino();
+  tick(timeStep) {
+    if (!this.tetromino) {
+      this.setTetromino();
+      this.tetromino.spawn(this);
+    }
+    this.tetromino.update(this);
   }
 
 };
 
 var GameboardUtils = {
   setTetromino() {
-    this.tetromino = RandomGenerator.getTetromino() 
-  },
+    var tetromino = RandomGenerator.getTetromino();
+
+    switch (tetromino) {
+      case 'I':
+        this.tetromino = Object.create(I);
+        break;
+      case 'J':
+        this.tetromino = Object.create(J);
+        break;
+      case 'L':
+        this.tetromino = Object.create(L);
+        break;
+      case 'O':
+        this.tetromino = Object.create(O);
+        break;
+      case 'S':
+        this.tetromino = Object.create(S);
+        break;
+      case 'T':
+        this.tetromino = Object.create(T);
+        break;
+      case 'Z':
+        this.tetromino = Object.create(Z);
+        break;
+    }
+  }
 }
 Object.setPrototypeOf(Gameboard, GameboardUtils);
 
@@ -81,10 +114,11 @@ var RandomGenerator = {
   },
   generateNewBag() {
     //var tetrominoes = ['I','J','L','O','S','T','Z'];
+    var tetrominoes = ['I','I','I','I','I','I','I'];
     var bag = [];
 
     for (var i = 7; i > 0; i--) {
-      var tetrominoIndex = Math.floor( Math.random() * i);
+      var tetrominoIndex = Math.floor(Math.random() * i);
 
       bag.push(tetrominoes[tetrominoIndex]);
       tetrominoes.splice(tetrominoIndex, 1);
@@ -94,25 +128,74 @@ var RandomGenerator = {
   }
 };
 
+var Tetromino = {
+  update(Gameboard) {
+    this.frames++;
+
+    // 60 should be the game speed instead.
+    if (this.frames != 60) return;
+
+    // Remove parts from the board
+    this.entity.forEach(function(part, index) {
+      var y = part.y + this.y,
+          x = part.x + this.x;
+
+      Gameboard.board[y][x] = 0;
+    }, this);
+
+    this.frames = 0;
+    this.y++;
+
+    // Add parts to the board
+    this.entity.forEach(function(part, index) {
+      var y = part.y + this.y,
+          x = part.x + this.x;
+      
+      Gameboard.board[y][x] = part.part;
+    }, this);
+  },
+  spawn(Gameboard) {
+    this.frames = 0;
+    this.y = this.spawnPoints.y;
+    this.x = this.spawnPoints.x;
+
+    this.entity.forEach(function(part, index) {
+      var y = part.y + this.y,
+          x = part.x + this.x;
+    
+      Gameboard.board[y][x] = part.part;
+    }, this);
+  }
+};
+
 var I = {
-  piece: [['I'],['I'],['I'],['I']]
+  entity: [
+    {
+      y: 0,
+      x: 0,
+      part: 'I'
+    },
+    {
+      y: 1,
+      x: 0,
+      part: 'I'
+    },
+    {
+      y: 2,
+      x: 0,
+      part: 'I'
+    },
+    {
+      y: 3,
+      x: 0,
+      part: 'I'
+    },
+  ],
+  spawnPoints: {
+    y: 0,
+    x: 5
+  }
 };
-var O = {
-  piece: [
-    [['O'],['O']],
-    [['O'],['O']]
-  ]
-};
-var T = {
-  piece: [
-    [['T'],['T'],['T']],
-    [[null],['T'],[null]]
-  ]
-};
+Object.setPrototypeOf(I, Tetromino);
 
-// setInterval(function() {
-//   Gameboard.setTetromino()
-//   console.log(Gameboard);
-// }, 1000);
-
-//requestAnimationFrame(game.main.bind(Game));
+requestAnimationFrame(Game.main.bind(Game));
