141500ef4dc5fc3156405243d8bda8626dcec551
Daniel Barbour <yayitsdan@Daniels-MacBook-Pro.local>
2017-08-19 11:51:41 -0400

    Rendering canvas, collision detection, user input

--- scripts/tetris.js
+++ scripts/tetris.js
@@ -4,14 +4,22 @@ var Game = {
   timeStep: 1000 / 60,
   lastFrameMs: 0,
   delta: 0,
+  init() {
+    UserInputs.init();
+    requestAnimationFrame(Game.main.bind(this));
+  },
   processInput() {
-
+    //console.log(UserInputs.inputqueue);
+    //UserInputs.inputqueue.pop();
   },
   update(timeStep) {
     Gameboard.tick(timeStep);
   },
   render(interp) {
-    //console.log(interp);
+    var canvas = document.getElementById('game');
+        ctx = canvas.getContext('2d');
+    
+    Asciirender.render();
   },
   main: function(timestamp) {
     var elapsed = timestamp - this.lastFrameMs;
@@ -27,6 +35,15 @@ var Game = {
 
     requestAnimationFrame(this.main.bind(this));
   }
+};
+
+var UserInputs = {
+  init() {
+    document.addEventListener('keyup', function(event) {
+      this.inputqueue.push(event.key);
+    }.bind(this));
+  },
+  inputqueue: []
 };
 
 // The game board state
@@ -63,8 +80,10 @@ var Gameboard = {
   tetromino: null,
   tick(timeStep) {
     if (!this.tetromino) {
+      console.log('set');
       this.setTetromino();
       this.tetromino.spawn(this);
+      console.log(this.tetromino);
     }
     this.tetromino.update(this);
   }
@@ -102,6 +121,33 @@ var GameboardUtils = {
 }
 Object.setPrototypeOf(Gameboard, GameboardUtils);
 
+var Asciirender = {
+  render() {
+    var asciiCanavs = document.getElementById('asciiGame');
+    
+    var frame = document.createElement('table');
+    frame.innerHTML = '<tbody id="frameBody"></tbody>';
+    var frameBody = frame.querySelector('#frameBody');  
+
+    asciiCanavs.innerHTML = '';
+
+    this.board.forEach(function(row) {
+      var asciiRow = document.createElement('tr');
+      row.forEach(function(column) {
+        var square = document.createElement('td');
+
+        if (column != 0) square.classList = column;
+
+        asciiRow.appendChild(square);
+      });
+      frameBody.appendChild(asciiRow);
+    }, this);
+
+    asciiCanavs.appendChild(frame);
+  }
+};
+Object.setPrototypeOf(Asciirender, Gameboard);
+
 // Handles randomly generating and returning a tetromino
 var RandomGenerator = {
   bag: [],
@@ -113,8 +159,7 @@ var RandomGenerator = {
     return this.bag.shift();
   },
   generateNewBag() {
-    //var tetrominoes = ['I','J','L','O','S','T','Z'];
-    var tetrominoes = ['I','I','I','I','I','I','I'];
+    var tetrominoes = ['I','J','L','O','S','T','Z'];
     var bag = [];
 
     for (var i = 7; i > 0; i--) {
@@ -129,42 +174,96 @@ var RandomGenerator = {
 };
 
 var Tetromino = {
+  move: {
+    x: 0,
+    y: 0
+  },
   update(Gameboard) {
     this.frames++;
+    this.handleInput();
 
-    // 60 should be the game speed instead.
-    if (this.frames != 60) return;
+    // If frames equals the game speed, increment it's y position
+    if (this.frames == 10) {
+      this.frames = 0;
+      if (!this.checkCollision(1,0)) {
+        this.move.y = 1;
+      } else {
+        console.log('remove');
+        Gameboard.tetromino = null;
+      }
+    }
 
-    // Remove parts from the board
-    this.entity.forEach(function(part, index) {
-      var y = part.y + this.y,
-          x = part.x + this.x;
-
-      Gameboard.board[y][x] = 0;
-    }, this);
-
-    this.frames = 0;
-    this.y++;
-
-    // Add parts to the board
-    this.entity.forEach(function(part, index) {
-      var y = part.y + this.y,
-          x = part.x + this.x;
-      
-      Gameboard.board[y][x] = part.part;
-    }, this);
+    this.clearSelf();
+    this.moveSelf();
   },
   spawn(Gameboard) {
     this.frames = 0;
-    this.y = this.spawnPoints.y;
-    this.x = this.spawnPoints.x;
 
     this.entity.forEach(function(part, index) {
-      var y = part.y + this.y,
-          x = part.x + this.x;
+      part.y += this.spawnPoints.y;
+      part.x += this.spawnPoints.x;
     
-      Gameboard.board[y][x] = part.part;
+      Gameboard.board[part.y][part.x] = part.part;
     }, this);
+  },
+  handleInput() {
+    var input = UserInputs.inputqueue.pop();
+
+    if (input == 'ArrowLeft') {
+      if (!this.checkCollision(0, 1)) {
+        this.move.x = -1;
+      }
+    }
+
+    if (input == 'ArrowRight') {
+      if (!this.checkCollision(1, 0)) {
+        this.move.x = 1;
+      }
+    }
+  },
+  checkCollision(y, x) {
+    var positions = [];
+    var collision = false;
+    this.entity.forEach(function(part) {
+      positions.push(part.y + ':' + part.x);
+    }, this);
+
+    this.entity.forEach(function(part, index) {
+      var partY = part.y + y,
+          partX = part.x + x;
+
+      // Is the part about to hit the floor?
+      if (Gameboard.board.length == partY) {
+        console.log('collision');
+        collision = true;
+        return
+      } 
+      
+      // Is the part going to an occupied space?
+      if (Gameboard.board[partY][partX] != 0) {
+      // Is the occupied space part of the current tetromino?
+        if (positions.indexOf(partY + ':' + partX) == -1) {
+          console.log('collision');
+          collision = true;
+        }
+      }
+    }, this);
+    return collision;
+  },
+  clearSelf() {
+    this.entity.forEach(function(part) {
+      Gameboard.board[part.y][part.x] = 0;
+    }, this);
+  },
+  moveSelf() {
+    this.entity.forEach(function(part) {
+      part.y += this.move.y;
+      part.x += this.move.x;
+      Gameboard.board[part.y][part.x] = part.part;
+    }, this);
+
+    this.move.y = 0;
+    this.move.x = 0;
   }
 };
 
@@ -176,18 +275,18 @@ var I = {
       part: 'I'
     },
     {
-      y: 1,
-      x: 0,
+      y: 0,
+      x: 1,
       part: 'I'
     },
     {
-      y: 2,
-      x: 0,
+      y: 0,
+      x: 2,
       part: 'I'
     },
     {
-      y: 3,
-      x: 0,
+      y: 0,
+      x: 3,
       part: 'I'
     },
   ],
@@ -198,4 +297,185 @@ var I = {
 };
 Object.setPrototypeOf(I, Tetromino);
 
-requestAnimationFrame(Game.main.bind(Game));
+var J = {
+  entity: [
+    {
+      y: 0,
+      x: 0,
+      part: 'J'
+    },
+    {
+      y: 0,
+      x: 1,
+      part: 'J'
+    },
+    {
+      y: 0,
+      x: 2,
+      part: 'J'
+    },
+    {
+      y: 1,
+      x: 2,
+      part: 'J'
+    }
+  ],
+  spawnPoints: {
+    y: 0,
+    x: 5
+  }
+};
+Object.setPrototypeOf(J, Tetromino);
+
+var L = {
+  entity: [
+    {
+      y: 0,
+      x: 0,
+      part: 'L'
+    },
+    {
+      y: 0,
+      x: 1,
+      part: 'L'
+    },
+    {
+      y: 0,
+      x: 2,
+      part: 'L'
+    },
+    {
+      y: 1,
+      x: 0,
+      part: 'L'
+    }
+  ],
+  spawnPoints: {
+    y: 0,
+    x: 5
+  }
+};
+Object.setPrototypeOf(L, Tetromino);
+
+var O = {
+  entity: [
+    {
+      y: 0,
+      x: 0,
+      part: 'O'
+    },
+    {
+      y: 0,
+      x: 1,
+      part: 'O'
+    },
+    {
+      y: 1,
+      x: 0,
+      part: 'O'
+    },
+    {
+      y: 2,
+      x: 0,
+      part: 'O'
+    }
+  ],
+  spawnPoints: {
+    y: 0,
+    x: 5
+  }
+};
+Object.setPrototypeOf(O, Tetromino);
+
+var S = {
+  entity: [
+    {
+      y: 0,
+      x: 1,
+      part: 'S'
+    },
+    {
+      y: 0,
+      x: 2,
+      part: 'S'
+    },
+    {
+      y: 1,
+      x: 1,
+      part: 'S'
+    },
+    {
+      y: 1,
+      x: 2,
+      part: 'S'
+    }
+  ],
+  spawnPoints: {
+    y: 0,
+    x: 5
+  }
+};
+Object.setPrototypeOf(S, Tetromino);
+
+var T = {
+  entity: [
+    {
+      y: 0,
+      x: 0,
+      part: 'T'
+    },
+    {
+      y: 0,
+      x: 1,
+      part: 'T'
+    },
+    {
+      y: 0,
+      x: 2,
+      part: 'T'
+    },
+    {
+      y: 1,
+      x: 1,
+      part: 'T'
+    }
+  ],
+  spawnPoints: {
+    y: 0,
+    x: 5
+  }
+};
+Object.setPrototypeOf(T, Tetromino);
+
+var Z = {
+  entity: [
+    {
+      y: 0,
+      x: 0,
+      part: 'Z'
+    },
+    {
+      y: 0,
+      x: 1,
+      part: 'Z'
+    },
+    {
+      y: 1,
+      x: 1,
+      part: 'Z'
+    },
+    {
+      y: 1,
+      x: 2,
+      part: 'Z'
+    }
+  ],
+  spawnPoints: {
+    y: 0,
+    x: 5
+  }
+};
+Object.setPrototypeOf(Z, Tetromino);
+
+Game.init();
+//requestAnimationFrame(Game.main.bind(Game));
